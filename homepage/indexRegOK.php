c7868d28a1d2caead988f28e8a22e82c3e1b3859
Diana <beldiman.diana@gmx.de>
2017-04-23 12:57:39 +0200

    Create indexRegOK.php

---
+++ homepage/indexRegOK.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+	<html lang="de">
+		<head>
+			<meta charset="utf-8">
+
+				<title>Gruppe 7</title>
+
+				<link rel="stylesheet" type="text/css" href="stylesheet.css">
+
+		</head>
+	<body>
+
+		<div class="container">
+
+			<header>
+			   <h1>Firma 7</h1>
+			</header>
+  
+		<nav>
+		  <ul>
+		    <li><a href="index.php">Startseite</a></li> 
+			<li><a href="produkt.html">Produkt</a></li>
+		    <li><a href="persoenlicheDaten.html">Persönliche Daten</a></li>
+		    <li><a href="impressum.html">Impressum</a></li>
+		  </ul>
+		</nav>
+
+		<article>
+		  <h1>Kurzpotrait</h1>
+			  
+			 <p>Die Firma 7 hat ein neues Produkt entwickelt. Die Anzahle der Mitarbeiter beträgt 3 Leute.<br/>
+				Das Produkt ist ein verschlüsseltes Messengersystem mit neuen Features.<br/> 
+				Die Produktentwicklung hat im Rahmen der INT Veranstaltung an der HS Mannheim stattgefunden.</p>
+
+		 <img src="mini2.gif" alt="HTML5 Icon" style="float:right;width:300px;height:250px;" >
+
+			<br/>
+		
+				<h2> Danke ! - Ihre Daten wurden gespeichert </h2> <br>
+			
+</article>
+
+
+<footer><p>Impressum</p>
+
+			<br/>
+
+			<p>Kontakt: Gruppe 7, Hochschule Mannheim, Speyererstrasse 10 </br>Gebäude A, Raum A107, Email: felixmorillas@gmail.com</p></footer>
+
+</div>
+
+	
+
+	</body>
+</html>
